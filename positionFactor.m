fa2363cb7768cabf7bd96ccfee290475d4516376
kimcd <christopher.kim@jhuapl.edu>
2022-11-23 11:27:09 -0500

    Update positionFactor.m

--- positionFactor.m
+++ positionFactor.m
@@ -1,9 +1,16 @@
 function r = positionFactor(x, y, a, b)
-% x = position
-% a = length (in x)
-% y = position
-% b = length (in y)
-
+% POSITIONFACTOR computes the relative component position factor, r, given 
+%                it's coordinate position on a board and the boards length 
+%                and width. r is used in Steinberg fatigue calculations. 
+%
+%   Inputs
+%       x = position
+%       a = length (in x)
+%       y = position
+%       b = length (in y)
+%
+%   C.Kim 22Nov2022 JHUAPL
+%%
 r = sin(pi * x / a) * sin(pi * y / b);
 
 end
